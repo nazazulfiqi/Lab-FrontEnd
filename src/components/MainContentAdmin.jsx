6a108161376e98a9c50008b6a283ffe23c21ab0c
Naza Zulfiqi <zulfiqinaza@gmail.com>
2023-01-01 17:37:47 +0700

    dashboard & page admin fix

--- src/components/MainContentAdmin.jsx
+++ src/components/MainContentAdmin.jsx
@@ -8,7 +8,7 @@ function MainContentAdmin() {
           <div class="col-12 d-flex justify-content-between align-items-center">
             <div class="dashboard-title-text">
               <h2>LAB SI/MI Dashboard</h2>
-              <p class="text-grey">Earning money online made easy.</p>
+              <p class="text-grey">On Insta @tutorgakids</p>
             </div>
             <button type="button" class="fs-18 text-grey-blue">
               <i class="fas fa-ellipsis-vertical"></i>
@@ -36,7 +36,7 @@ function MainContentAdmin() {
                     </span>
                   </div>
                   <div class="item-title text-cyan fs-4 fw-6 mb-1">
-                    R91 <span class="text-blue">Account credit</span>
+                    Toks <span class="text-blue">Lab Honor</span>
                   </div>
                   <div class="item-bottom d-flex align-items-center justify-content-between">
                     <div class="earnings me-3">
@@ -44,7 +44,7 @@ function MainContentAdmin() {
                         <i class="fa-solid fa-arrow-right"></i>
                       </span>
                       <span class="fs-15">
-                        Earned <span class="text-cyan">R0</span> today{" "}
+                        Earned <span class="text-cyan">1Jt</span> today{" "}
                       </span>
                     </div>
                     <div class="deposit">
@@ -56,67 +56,6 @@ function MainContentAdmin() {
                     <span class="balance-icon align-self-end fs-20">
                       <i class="fa-solid fa-wallet"></i>
                     </span>
-                  </div>
-                </div>
-              </a>
-            </div>
-
-            <div class="col-md-6 col-lg-4">
-              <a href="#" class="text-decoration-none">
-                <div class="overview-section-item bg-white p-4">
-                  <div class="item-top d-flex align-items-center justify-content-between mb-3">
-                    <p class="text-grey mb-0">Account Rank</p>
-                    <span class="info-icon d-flex align-items-center justify-content-center bg-grey text-white fs-8">
-                      <i class="fa-solid fa-question"></i>
-                    </span>
-                  </div>
-                  <div class="item-title text-cyan fs-4 fw-bold mb-1">
-                    <span>
-                      <i class="fas fa-award"></i>
-                    </span>
-                    <span class="text-blue fs-4 fw-6">Silver Rank</span>
-                  </div>
-                  <div class="item-bottom d-flex align-items-center justify-content-between">
-                    <div class="earnings me-3">
-                      <span class="text-grey-blue">
-                        <i class="fas fa-signal"></i>
-                      </span>
-                      <span class="fs-15">
-                        Earned <span class="text-cyan">45</span> until rank
-                        expires{" "}
-                      </span>
-                    </div>
-                    <span class="balance-icon align-self-end fs-20">
-                      <i class="fa-solid fa-wallet"></i>
-                    </span>
-                  </div>
-                </div>
-              </a>
-            </div>
-
-            <div class="col-md-6 col-lg-4">
-              <a href="#" class="text-decoration-none">
-                <div class="overview-section-item bg-white p-4">
-                  <div class="item-title text-cyan fs-3 fw-bold mb-1">
-                    <span>
-                      <i class="fa-solid fa-network-wired"></i>
-                    </span>
-                    <span class="text-blue fs-4 fw-6">Bonus</span>
-                  </div>
-                  <div class="item-bottom d-flex align-items-center justify-content-between">
-                    <div class="earnings me-3">
-                      <span class="text-grey">
-                        You have{" "}
-                        <span class="text-grey-blue">2 downlines </span>
-                        <span class="text-blue">Withdraw</span>
-                      </span>
-                    </div>
-                    <button
-                      type="button"
-                      class="btn-outline-sm text-blue d-block mt-2 px-2"
-                    >
-                      Copy referral link
-                    </button>
                   </div>
                 </div>
               </a>
