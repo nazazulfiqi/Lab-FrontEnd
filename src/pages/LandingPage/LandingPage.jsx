8e9d4d562d38296547706808597a06d0cf2fc580
Naza Zulfiqi <zulfiqinaza@gmail.com>
2023-02-26 21:49:03 +0700

    bg fix

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -8,12 +8,16 @@ import NavbarComponent from "../../components/LandingComponent/NavbarComponent";
 import Perlengkapan from "../../components/LandingComponent/Perlengkapan";
 import Struktur from "../../components/LandingComponent/Struktur";
 import TataTertib from "../../components/LandingComponent/TataTertib";
+import backgroundHome from "../../../public/assets/images/accsentbg.png";
 import "./landingpage.css";
 
 function LandingPage() {
   return (
     <div className="wrapper-home">
-      <div className="container-fluid container-pagehome p-0">
+      <div
+        className="container-fluid container-pagehome p-0"
+        style={{ backgroundImage: `url(${backgroundHome})` }}
+      >
         <NavbarComponent />
         <div className="container container-home d-flex align-items-center">
           <div className="row mx-0">
