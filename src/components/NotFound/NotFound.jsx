3e54a11e347af530e173d39f276d1a9d3d70fdfd
Naza Zulfiqi <zulfiqinaza@gmail.com>
2023-10-04 12:24:31 +0700

    improvement: add routes 404

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="container" id="hero">
+      <div className="row">
+        <div className="col-lg-6 text-white">
+          <h1 className="title-hero">NOT FOUND</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
